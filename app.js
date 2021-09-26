39f012971190c0e7213298e5b16a2d7b2af5a0fc
bubai14 <bubaibal14@gmail.com>
2021-09-26 21:31:19 +0530

    Updated the message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Sample app deployed using AWS DevOps!'));
+app.get('/', (req, res) => res.send('Added a review stage in the pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
